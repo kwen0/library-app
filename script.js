cba80de30bd614491b982a4396e14911ddabc0a3
Karen <karen.wen3@gmail.com>
2021-11-11 15:29:10 -0500

    added toggle read status function, updated style

--- script.js
+++ script.js
@@ -1,24 +1,14 @@
+const submitBtn = document.querySelector("#submit");
+const modal = document.querySelector(".modal");
+const table = document.querySelector("tbody");
 let myLibrary = [];
 
 function Book(title, author, status) {
-    this.Title = title;
-    this.Author = author;
-    this.Status = status;
+    this.title = title;
+    this.author = author;
+    this.status = status;
     myLibrary.push(this);
 }
-
-const submitBtn = document.querySelector("#submit");
-submitBtn.addEventListener('click', () => {
-    if (document.querySelector("#title").value == "" || 
-    document.querySelector("#author").value == "" || 
-    document.querySelector("#status").value == "") {
-        alert("Please fill in all the fields")
-    } else {
-        addBookToLibrary();
-        addBookToTable(); 
-        resetInputs();
-    }
-});
 
 function addBookToLibrary() {
     let title = document.querySelector("#title").value;
@@ -27,74 +17,96 @@ function addBookToLibrary() {
     const book = new Book(title, author, status);
 }
 
+function generateTable(array) {
+    for (let key of array) {
+        let row = table.insertRow();
+
+        let col1 = row.insertCell();
+        let titleCol = document.createTextNode(`${key.title}`);
+        col1.appendChild(titleCol);
+
+        let col2 = row.insertCell();
+        let authorCol = document.createTextNode(`${key.author}`);
+        col2.appendChild(authorCol);
+
+        let col3 = row.insertCell();
+        let statusBtn = document.createElement("button");
+        statusBtn.innerHTML = `${key.status}`;
+        statusBtn.classList.add("status");
+        col3.appendChild(statusBtn);
+
+        let col4 = row.insertCell();
+        let delBtn = document.createElement("button");
+        delBtn.innerHTML = "Remove";
+        delBtn.classList.add("remove");
+        col4.appendChild(delBtn)
+    }
+}
+
+function addBookToTable() {
+    clearTable();
+    generateTable(myLibrary)
+}
+
+function clearTable() {
+    for (let i = 0; i < table.rows.length;) {table.deleteRow(i)}
+}
+
 function resetInputs() {
     document.querySelector("#title").value=null;
     document.querySelector("#author").value=null;
     document.querySelector("#status").value=null;
 }
 
-const modal = document.querySelector(".modal");
-addBtn.addEventListener('click', () => modal.style.display = "block");
-cancel.addEventListener('click', () => modal.style.display = "none");
-
-const book1 = new Book("Pride and Prejudice", "Jane Austen", "Want to read");
-const book2 = new Book("Malibu Rising", "Taylor Jenkins Reid", "Read");
-const book3 = new Book("Think Like a Programmer", "V. Anton Spraul", "Currently reading");
-  
-function generateTable() {
-    for (let book of myLibrary) {
-      let row = table.insertRow();
-      for (key in book) {
-        let cell = row.insertCell();
-        let text = document.createTextNode(book[key]);
-        cell.appendChild(text);
-      }
-      let cell = row.insertCell();
-      let delBtn = document.createElement("button");
-      delBtn.innerHTML = "Remove";
-      cell.appendChild(delBtn)
-    }
-  }
-  
-const table = document.querySelector("table");
-const data = Object.keys(myLibrary[0]);
-
-generateTable();
-
-function addBookToTable() {
-    let bookToAdd = myLibrary[myLibrary.length - 1]
-    let row = table.insertRow();
-    for (key in bookToAdd) {
-        let cell = row.insertCell();
-        let text = document.createTextNode(bookToAdd[key]);
-        cell.appendChild(text);
-    }
-    let cell = row.insertCell();
-    let delBtn = document.createElement("button");
-    delBtn.innerHTML = "Remove";
-    cell.appendChild(delBtn)
-}
-
-
-table.addEventListener("click", (e) => {
-    const currentTarget = e.target.parentNode.parentNode.childNodes[0]
-    if (e.target.innerHTML == "Remove") {
-        if (confirm(`are you sure you want to delete ${currentTarget.innerText}`))
-          deleteBook(findBook(currentTarget.innerText));
-      }
-  });
-
-function deleteBook(book) {
-    myLibrary.splice(book, 1);
-    table.deleteRow(book+1);
-}
-
 function findBook(title) {
     for (book of myLibrary) {
-        if (book.Title === title) {
-            console.log(myLibrary.indexOf(book))
+        if (book.title === title) {
             return myLibrary.indexOf(book);
         }
     }
 }
 
+function deleteBook(book) {
+    myLibrary.splice(book,1);
+    table.deleteRow(book);
+}
+
+function toggleStatus(book) {
+    if (myLibrary[book].status == "Read") {
+        myLibrary[book].status = "Want to read";
+    } else
+        myLibrary[book].status = "Read";
+}
+
+addBtn.addEventListener('click', () => modal.style.display = "block");
+cancel.addEventListener('click', () => modal.style.display = "none");
+
+submitBtn.addEventListener('click', () => {
+    if (document.querySelector("#title").value == "" || 
+    document.querySelector("#author").value == "" || 
+    document.querySelector("#status").value == "") {
+        alert("Please fill in all the fields");
+    } else {
+        addBookToLibrary();
+        addBookToTable(); 
+        resetInputs();
+    }
+});
+
+table.addEventListener("click", (e) => {
+    const currentTarget = e.target.parentNode.parentNode.childNodes[0]
+    if (e.target.classList.contains("remove")) {
+        deleteBook(findBook(currentTarget.innerText));
+    }
+    if (e.target.classList.contains("status")) {
+        toggleStatus(findBook(currentTarget.innerText));
+        e.target.innerHTML = myLibrary[findBook(currentTarget.innerText)].status;
+    }
+  });
+
+//default library
+const book1 = new Book("Pride and Prejudice", "Jane Austen", "Want to read");
+const book2 = new Book("Malibu Rising", "Taylor Jenkins Reid", "Read");
+const book3 = new Book("Think Like a Programmer", "V. Anton Spraul", "Want to read");
+const book4 = new Book("Harry Potter and the Sorcerer's Stone", "J.K. Rowling", "Read")
+generateTable(myLibrary);

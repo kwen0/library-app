ff64d0adff4ab65459f723ed997babcab43a1e36
Karen <karen.wen3@gmail.com>
2021-11-10 19:39:57 -0500

    Added remove function and updated style

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
 let myLibrary = [];
 
 function Book(title, author, status) {
-    this.title = title;
-    this.author = author;
-    this.status = status;
+    this.Title = title;
+    this.Author = author;
+    this.Status = status;
     myLibrary.push(this);
 }
 
 const submitBtn = document.querySelector("#submit");
-submitBtn.addEventListener('click', () => {addBookToLibrary(), resetInputs()});
+submitBtn.addEventListener('click', () => {
+    if (document.querySelector("#title").value == "" || 
+    document.querySelector("#author").value == "" || 
+    document.querySelector("#status").value == "") {
+        alert("Please fill in all the fields")
+    } else {
+        addBookToLibrary();
+        addBookToTable(); 
+        resetInputs();
+    }
+});
 
 function addBookToLibrary() {
     let title = document.querySelector("#title").value;
     let author = document.querySelector("#author").value;
     let status = document.querySelector("#status").value;
     const book = new Book(title, author, status);
-    const row = document.createElement("p")
-    row.textContent = book.title
-    listOfBooks.appendChild(row)
 }
 
 function resetInputs() {
@@ -27,15 +34,67 @@ function resetInputs() {
 }
 
 const modal = document.querySelector(".modal");
-const searchbar = document.querySelector("#searchbar");
-const closeBtn = document.querySelector(".close");
-searchbar.addEventListener('click', () => modal.style.display = "block");
-closeBtn.addEventListener('click', () => modal.style.display = "none");
-
+addBtn.addEventListener('click', () => modal.style.display = "block");
+cancel.addEventListener('click', () => modal.style.display = "none");
 
 const book1 = new Book("Pride and Prejudice", "Jane Austen", "Want to read");
 const book2 = new Book("Malibu Rising", "Taylor Jenkins Reid", "Read");
-const book3 = new Book("Think Like a Programmer: An Introduction to Creative Problem Solving", "V. Anton Spraul", "Currently reading");
+const book3 = new Book("Think Like a Programmer", "V. Anton Spraul", "Currently reading");
+  
+function generateTable() {
+    for (let book of myLibrary) {
+      let row = table.insertRow();
+      for (key in book) {
+        let cell = row.insertCell();
+        let text = document.createTextNode(book[key]);
+        cell.appendChild(text);
+      }
+      let cell = row.insertCell();
+      let delBtn = document.createElement("button");
+      delBtn.innerHTML = "Remove";
+      cell.appendChild(delBtn)
+    }
+  }
+  
+const table = document.querySelector("table");
+const data = Object.keys(myLibrary[0]);
 
-const listOfBooks = document.querySelector("#listOfBooks")
+generateTable();
+
+function addBookToTable() {
+    let bookToAdd = myLibrary[myLibrary.length - 1]
+    let row = table.insertRow();
+    for (key in bookToAdd) {
+        let cell = row.insertCell();
+        let text = document.createTextNode(bookToAdd[key]);
+        cell.appendChild(text);
+    }
+    let cell = row.insertCell();
+    let delBtn = document.createElement("button");
+    delBtn.innerHTML = "Remove";
+    cell.appendChild(delBtn)
+}
+
+
+table.addEventListener("click", (e) => {
+    const currentTarget = e.target.parentNode.parentNode.childNodes[0]
+    if (e.target.innerHTML == "Remove") {
+        if (confirm(`are you sure you want to delete ${currentTarget.innerText}`))
+          deleteBook(findBook(currentTarget.innerText));
+      }
+  });
+
+function deleteBook(book) {
+    myLibrary.splice(book, 1);
+    table.deleteRow(book+1);
+}
+
+function findBook(title) {
+    for (book of myLibrary) {
+        if (book.Title === title) {
+            console.log(myLibrary.indexOf(book))
+            return myLibrary.indexOf(book);
+        }
+    }
+}
 
